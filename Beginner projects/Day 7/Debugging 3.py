f653d0682c560fb2a6a52acba37968af61a549bd
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-05 18:20:33 +0100

    Create Debugging 3.py

---
+++ Beginner projects/Day 7/Debugging 3.py
@@ -0,0 +1,10 @@
+
+# Debug Odd or Even exercise
+
+number = int(input("Which number do you want to check? "))
+
+# use == instead of =
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")

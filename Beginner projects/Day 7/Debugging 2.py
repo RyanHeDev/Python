c8d0ad6c93d9d76d529df5f7c84a3f0ae3714383
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-05 18:19:38 +0100

    Create Debugging 2.py

---
+++ Beginner projects/Day 7/Debugging 2.py
@@ -0,0 +1,16 @@
+
+#Debugging Leap Year Exercise
+# add int()
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leep year.")
+        else:
+            print("Not a leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not a leap year.")

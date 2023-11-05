79c7b24f28911ec172a9602dfbe5dea56d073fee
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-05 17:52:20 +0100

    Create Project 4.py

---
+++ Beginner projects/Day2/Project 4.py
@@ -0,0 +1,9 @@
+
+# Leap Year
+
+year = int(input("Which year do you want to check? "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap year")
+else:
+    print("Not a leap year") 

7b8c503707af690c5de7502769dd24bafafa602b
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-05 18:14:10 +0100

    Create Project 3.py

---
+++ Beginner projects/Day6/Project 3.py
@@ -0,0 +1,17 @@
+
+travel_log = {
+    "Morocco" : {
+        "cities_visited":  ["Rabat"], "total_visits": 20
+    },
+}
+
+new_travel_log = [
+   {
+   "country": "Morocco",
+    "cities_visited":  ["Rabat"], 
+    "total_visits": 20
+    },
+]
+
+print(travel_log)
+print(new_travel_log)

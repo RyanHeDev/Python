bf3a8677af6412983f4549a4b5349fe978e91aff
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-05 18:21:54 +0100

    Create Debugging 6.py

---
+++ Beginner projects/Day 7/Debugging 6.py
@@ -0,0 +1,9 @@
+
+# Play Computer
+
+year = int(input("What's your year of birth: "))
+if year > 1980 and year < 1994:
+    print("You are a millenial.")
+    #add >=  instead of >
+elif year >= 1994:
+    print("You are a Gen Z.")

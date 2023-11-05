ba234a1d2343ba9de9738ed36d3c8e604288f946
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-05 18:13:33 +0100

    Create Project 1.py

---
+++ Beginner projects/Day6/Project 1.py
@@ -0,0 +1,28 @@
+
+# Secret Auction Program 
+# S.A.P
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} they won with a bid of ${highest_bid}")
+    
+while not bidding_finished:
+    print("Welcome to the secret auction program.")
+    name = input("What is your name?: ")
+    price = int(input("what's your bid? $"))
+    bids[name] = price
+    should_continue = input("Are there any bidders? Type 'yes' or 'no'.\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")

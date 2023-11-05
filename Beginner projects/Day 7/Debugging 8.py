e1bf03c4e1679cfcc6fae67d937de7495badb707
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-05 18:22:37 +0100

    Create Debugging 8.py

---
+++ Beginner projects/Day 7/Debugging 8.py
@@ -0,0 +1,10 @@
+
+# add int
+age = int(input("How old are you: "))
+# add >= 
+if age >= 18:
+    # add f and fix indentation 
+    print(f"You can drive at age {age}.")
+    # else 
+else:
+    print(f" You can't drive at age {age}.")

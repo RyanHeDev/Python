a18f951f6d95b55c3217363979fd450af66f0183
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-06 15:42:20 +0100

    Update and rename Runner game.py to Template.py

---
+++ Pygame/Projects/Template.py
@@ -0,0 +1,17 @@
+import pygame 
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+# Displays the name of the app
+pygame.display.set_caption("Runner")
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+    clock.tick(60)

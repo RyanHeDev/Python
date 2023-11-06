d2a642f2e5d126e0730cfa8fac70377254fb8fa6
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-06 15:43:38 +0100

    Create tuple.py

---
+++ Intermidiate projects/Day 2/tuple.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("black","green")
+timmy.fd(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()

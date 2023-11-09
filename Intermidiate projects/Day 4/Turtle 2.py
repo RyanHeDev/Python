025e99584e6c328a62abdc8d2182b154bfb7ade4
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-09 17:42:07 +0100

    Create Turtle 2.py

---
+++ Intermidiate projects/Day 4/Turtle 2.py
@@ -0,0 +1,28 @@
+from turtle import Turtle , Screen
+
+tim = Turtle()
+tim.shape("turtle")
+for _ in range(15):
+    tim.fd(10)
+    tim.penup()
+    tim.fd(10)
+    tim.pendown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()

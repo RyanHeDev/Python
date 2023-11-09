fbc064e6b4c913bef4a449031de4ea6fa776c93c
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-09 17:47:37 +0100

    Create Colorgram.py

---
+++ Intermidiate projects/Day 4/Colorgram.py
@@ -0,0 +1,14 @@
+import colorgram
+
+rgb_colors = []
+colors = colorgram.extract("Trash_art.jpg", 25)
+
+for color in colors:
+  r = color.rgb.r
+  g = color.rgb.g
+  b = color.rgb.b
+  new_color = (r, g, b)
+  rgb_colors.append(new_color)
+
+print(rgb_colors)
+  

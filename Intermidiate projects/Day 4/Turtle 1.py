87cab039f2aefd6dd2e23f729b788ea8d6e55f66
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-09 17:41:05 +0100

    Create Turtle 1.py

---
+++ Intermidiate projects/Day 4/Turtle 1.py
@@ -0,0 +1,21 @@
+from turtle import Turtle , Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("black", "cyan")
+i = 0
+while i < 4:
+    i += 1
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()

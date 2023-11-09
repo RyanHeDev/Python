9c2a14e7abe0541d032a48c95977f2095277b2c1
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-09 17:44:42 +0100

    Create Turtle 8.py

---
+++ Intermidiate projects/Day 4/Turtle 8.py
@@ -0,0 +1,25 @@
+from turtle import Turtle , Screen
+import random
+
+tim = Turtle()
+tim.shape("turtle")
+tim.home()
+
+Screen = Screen()
+Screen.bgcolor("black")
+
+colors = ["Green","Blue","Yellow","Cyan","Orange","Red","White"]
+
+tim.pensize(15)
+tim.speed("fastest")
+directions = [0, 90, 180, 270]
+
+for _ in range(200):
+    tim.color(random.choice(colors))
+    tim.fd(30)
+    tim.setheading(random.choice(directions))
+
+
+    
+    
+Screen.exitonclick()

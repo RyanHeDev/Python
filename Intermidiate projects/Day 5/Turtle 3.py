1f044b4efd64ce6494b3b76f7c936999cef1553d
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-09 19:17:13 +0100

    Update Turtle 3.py

--- Intermidiate projects/Day 5/Turtle 3.py
+++ Intermidiate projects/Day 5/Turtle 3.py
@@ -10,22 +10,31 @@ def move_forwards():
     tim.forward(10)
 
 def move_backwards():
-    tim.forward(-10)
+    tim.backward(10)
 
 
 def turn_right():
-    tim.rt(10)
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
     
 
 def turn_left():
-    tim.lt(10)
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
 
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+    
+    
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=turn_right)
 screen.onkey(key="d", fun=turn_left)
-Screen.onkey(key="c",fun=clear())
+screen.onkey(key="c",fun=clear)
 
 
 

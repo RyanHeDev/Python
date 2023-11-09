3671c17c22b916604050640967e02f5f76500b9d
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-09 17:42:37 +0100

    Create Turtle 3.py

---
+++ Intermidiate projects/Day 4/Turtle 3.py
@@ -0,0 +1,28 @@
+from turtle import Turtle , Screen
+
+tim = Turtle()
+tim.shape("turtle")
+for _ in range(10):
+    tim.fd(10)
+    tim.color("white")
+    tim.fd(10)
+    tim.color("black")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()

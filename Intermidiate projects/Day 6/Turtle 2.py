e269afa32960d754cb66800cc8f006a1f29d3005
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-10 17:14:06 +0100

    Create Turtle 2.py

---
+++ Intermidiate projects/Day 6/Turtle 2.py
@@ -0,0 +1,26 @@
+from turtle import Screen, Turtle
+
+# Screen setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+# creating a Snake body
+
+starting_positions = [(0,0), (-20,0), (-40,0)]
+
+for position in starting_positions:
+  new_segment = Turtle("square")
+  new_segment.color("white")
+  new_segment.goto(position)
+  
+
+
+
+
+
+
+
+screen.exitonclick()
+

8870c887a1fb765cf974c98318e2bc08ef1bce31
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-11 12:57:16 +0100

    Create Odd and even.py

---
+++ my_learning/Odd and even.py
@@ -0,0 +1,15 @@
+# odd and even
+
+
+def isOdd(number):
+    if number % 2 == 0:
+        return False
+
+
+def isEven(number):
+    if number % 2 == 0:
+        return True
+        
+
+print(isOdd(42))
+print(isEven(42))

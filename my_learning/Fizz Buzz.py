17c40fe6363ac956c4d6d1862b00d23b3b600601
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-11 13:31:07 +0100

    Create Fizz Buzz.py

---
+++ my_learning/Fizz Buzz.py
@@ -0,0 +1,15 @@
+# Fizz Buzz
+
+def fizzBuzz(upTo):
+    for number in range(1,  upTo + 1):
+        if number % 3 == 0 and number % 5 == 0: # or num % 15 == 0
+            print("FizzBuzz", end='')
+        elif number % 3 == 0:
+            print("Fizz", end='')
+        elif number % 5 == 0:
+            print("Buzz", end='')
+        else:
+            print(number, end='')
+
+
+fizzBuzz(32)

3a1860a559469447be931901513930424600e9d9
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-11 12:46:35 +0100

    Create temperature converter.py

---
+++ my_learning/temperature converter.py
@@ -0,0 +1,11 @@
+# Temperature Conversion
+
+def convertToFahrenheit(Celsius):
+    return Celsius * (9/5) + 32
+
+def convertToCelsius(Fahrenheit):
+    return (Fahrenheit- 32) * (5/9)
+
+
+print(convertToFahrenheit(0))
+print(convertToCelsius(0))

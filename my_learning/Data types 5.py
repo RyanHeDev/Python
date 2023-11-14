cf89ea096376817545cdb48cded3af50faaaf282
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-14 18:49:09 +0100

    Create Data types 5.py

---
+++ my_learning/Data types 5.py
@@ -0,0 +1,7 @@
+
+colors = ["red","cyan","lime","blue","green","white","black","purple","pink","brown"]
+
+starting_number = int(input("Enter a starting number between 0 and 4: "))
+ending_number = int(input("Enter an ending number between 5 and 9: "))
+
+print(colors[starting_number:ending_number])

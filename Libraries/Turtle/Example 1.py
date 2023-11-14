7267894cabbf0ead88cd6c182cc5cd7594f07370
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2023-11-14 17:47:16 +0100

    Create Example 1.py

---
+++ Libraries/Turtle/Example 1.py
@@ -0,0 +1,12 @@
+import turtle 
+
+turtle.shape("turtle")
+turtle.color("cyan")
+turtle.speed(0)
+turtle.bgcolor("black")
+
+for i in range(0, 4):
+    turtle.fd(100)
+    turtle.rt(90)
+
+turtle.exitonclick()
